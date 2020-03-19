6f00aba88b8669d2a7c64910e25c727f2216395e
Ana Berrocal <anaberrocalmail@gmail.com>
2020-03-19 19:23:02 -0400

    Update styling and logo for FindTheFood plus add responsiveness to logo

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import BusinessList from '../BusinessList/BusinessList.js';
 import SearchBar from '../SearchBar/SearchBar.js';
 import Yelp from '../../util/Yelp.js';
+import logo from './findthefood.png';
 
 
 class App extends React.Component {
@@ -22,9 +23,13 @@ class App extends React.Component {
   render() {
   return (
     <div className="App">
-    <h1>ravenous</h1>
+      <div className="header">
+      <img className="logo" src={logo}/>
+    <h1>FIND<span className="the">THE</span>FOOD</h1>
+    </div>
     {<SearchBar searchYelp={this.searchYelp} />}
     {<BusinessList businesses={this.state.businesses} />}
+    <div id="footer-container"><footer className="footer">Made with <span>❤️</span> by <a target="_blank" href="https://anaberrocal.com/">Ana Berrocal</a></footer></div>
     </div>
       );
     }
