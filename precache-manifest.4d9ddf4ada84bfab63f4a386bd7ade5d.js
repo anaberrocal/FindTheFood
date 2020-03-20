9415e87044056a1a10750f53d6c5f9ede2ea59dd
Ana Berrocal <anaberrocalmail@gmail.com>
2020-03-19 22:27:02 -0400

    deploy test

---
+++ precache-manifest.4d9ddf4ada84bfab63f4a386bd7ade5d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "984819dd6d0ff128bd8262c8d0df5df0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7cbfd5def602b31a8e4",
+    "url": "/static/css/main.fe558f04.chunk.css"
+  },
+  {
+    "revision": "5b569ed09d7f78128b20",
+    "url": "/static/js/2.95006776.chunk.js"
+  },
+  {
+    "revision": "c7cbfd5def602b31a8e4",
+    "url": "/static/js/main.1242f0ca.chunk.js"
+  },
+  {
+    "revision": "6f32849b6e5cbfe83fe7",
+    "url": "/static/js/runtime-main.cb8d49e7.js"
+  },
+  {
+    "revision": "1ffb30e7c84859dde5e0f3ad6be91855",
+    "url": "/static/media/findthefood.1ffb30e7.png"
+  },
+  {
+    "revision": "c4b13970ec1fd394162e3c24f6b560b8",
+    "url": "/static/media/findthefood_desktop.c4b13970.png"
+  },
+  {
+    "revision": "a9b29c4d8605755d729d300b9a8113b2",
+    "url": "/static/media/findthefood_mobile.a9b29c4d.png"
+  }
+]);
