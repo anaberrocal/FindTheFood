f860178fd2da703a9fdfbc014aa46799a65a3241
Ana Berrocal <anaberrocalmail@gmail.com>
2020-07-04 16:36:55 -0400

    Change rating display for bussiness component

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -18,7 +18,7 @@ class Business extends React.Component {
     </div>
     <div className="Business-reviews">
       <h3>{this.props.business.category}</h3>
-      <h3 className="rating">{this.props.business.rating}</h3>
+      <h3 className="rating">{this.props.business.rating} stars</h3>
       <p>{this.props.business.reviewCount} reviews </p>
     </div>
   </div>
